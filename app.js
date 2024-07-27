b43ed3fe0839eb08e0eae4edf1b5b1c70961f8a7
Ganesh Rodge <ganeshrodge25@gmail.com>
2024-07-27 08:45:05 +0530

    start work on JS file

---
+++ app.js
@@ -0,0 +1,11 @@
+let start = document.querySelector("hour");
+let stop = document.querySelector("#stop");
+let reset = document.querySelector("#reset")
+
+let hours = document.querySelector("#hour");
+let minutes = document.querySelector("#minute");
+let seconds = document.querySelector("second");
+
+const resetTime = () =>{
+    
+}
